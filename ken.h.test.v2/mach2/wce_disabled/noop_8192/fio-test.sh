25d807485e9570f485ef114a2439f8f9914abce7
John Suykerbuyk <john.suykerbuyk@seagate>
2020-05-11 08:10:42 -0600

    New Mach2 & Evans FIO datasets
    The test results include every permutation a 12 disk array LVM testing for:
    - Evans and Mach2
    - deadline & noop scheduler
    - read_ahead_kb of 4k, 32k, 64k, and 128k
    - fio que depth of 1 4 8 16 32 64 128 256 512 1024
    - fio test: read, write, randrw, randread, randwrite
    - fio block size of 4k, 8k, 32k, 128k, 256k, 512k, 1024k, 2048k, 4096k
    - fio job count was held fixed at "1"
    - We have both the EVANS configuration with simple LVM volumes atop a single disk drive, AND we have an LVM config that closely resembles the Mach2 RAID0 striping (in evans_paired)
    
    New fio test script:
    - ../scripts/fio-test-with-lvm-setup.sh
    Uses new lvm configuration library script:
    - ../scripts/lvm_funcs.sh

---
+++ ken.h.test.v2/mach2/wce_disabled/noop_8192/fio-test.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+
+rm -f *.map *.log
+source ./lvm_funcs.sh
+
+#DISK_READ_AHEAD=4096
+DISK_READ_AHEAD=8192
+#DISK_READ_AHEAD=32768
+#DISK_READ_AHEAD=65536
+#DISK_READ_AHEAD=131072
+#DISK_SCHEDULER='deadline'
+DISK_SCHEDULER='noop'
+
+do_fio_test() {
+	TDIR=$1
+	MYTGT=$2
+	TNAME=$3
+	if [[ ! -d $TDIR ]]; then
+		mkdir -p "$TDIR"
+	fi
+	cd $TDIR
+	JOBS=1
+	for PAT in 'read' 'write' 'randrw' 'randread' 'randwrite'; do
+		for BLK in 4k 8k 16k 32k 128k 256k 512k 1024k 2048k 4096k; do
+			for QUE in 1 4 8 16 32 64 128 256 512 1024; do
+				echo "running $PAT with $BLK and iodepth=$QUE fio against $MYTGT"
+				fio --filename=$MYTGT \
+				    --rw=$PAT \
+				    --ioengine=libaio \
+				    --iodepth=$QUE \
+				    --numjobs=$JOBS \
+				    --direct=1 \
+				    --runtime=120 \
+				    --name="$PAT-$BLK" \
+				    --group_reporting  \
+				    --bs=$BLK \
+				    --output-format=json | tee  "$TNAME-$PAT-$BLK-$QUE-$JOBS.fio.json"
+			done
+		done
+	done
+}
+
+mk_lvms | tee test.log
+for DTYPE in mach2 evans
+do
+	TDIR="${DTYPE}/${DISK_SCHEDULER}_${DISK_READ_AHEAD}"
+	while read TGT
+	do
+		TNAME=$(basename $TGT)
+		do_fio_test $TDIR $TGT $TNAME &	
+	done <LVM_${DTYPE}.map
+done
+
