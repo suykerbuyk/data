7c8d8339c53830a24ce25b2d81d3771f09d22c81
John-S <git@suykerbuyk.org>
2020-05-11 11:57:52 -0600

    Built Parser for JSON files
    Fixed FIO warning message in evans_paired data set with:
    - for x in $(ls *.json) ; do sed -i '/^fio: /d' $x; done

---
+++ ken.h.test.v2/parse-fio-json.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+
+DIRS="mach2/wce_disabled/noop_4096 \
+      mach2/wce_disabled/noop_131072 \
+      mach2/wce_disabled/deadline_65536 \
+      mach2/wce_disabled/deadline_4096 \
+      mach2/wce_disabled/noop_32768 \
+      mach2/wce_disabled/noop_8192 \
+      evans_paired/wce_enabled/deadline_4096 \
+      evans/wce_enabled/deadline_4096"
+
+
+for DIR in $DIRS
+do
+	DRV_PLATFORM="$(echo $DIR | awk -F '/' '{print $1}' | sed 's/ //g')"
+	DRV_WCE="$(echo $DIR | awk -F '/' '{print $2}' | sed 's/ //g')"
+	DRV_SCHEDULER="$(echo $DIR | awk -F '/' '{print $3}' | sed 's/ //g')"
+	#TEST_NAME="$(echo $DIR | sed 's/\//-/g')"
+	TEST_NAME="${DRV_PLATFORM}.${DRV_WCE}.${DRV_SCHEDULER}"
+	CSV_FILE="${TEST_NAME}.csv"
+	echo "Creating: ${CSV_FILE}"
+	HEADER="config,timestamp,fio_pattern,ioengine,direct,iodepth,job_name"
+	HEADER="${HEADER},blocksize,numjobs,runtime_secs,read_io_bytes"
+	HEADER="${HEADER},read_bw_bytes,read_bw_mean,read_iops_mean,write_io_bytes"
+	HEADER="${HEADER},write_bw_bytes,write_bw_mean,write_iops_mean"
+	echo "${HEADER}">"${CSV_FILE}"
+	for SRC_FILE in $(ls ${DIR}/*.json)
+	do
+        	echo -n "${TEST_NAME}," >>"${CSV_FILE}"
+		cat "${SRC_FILE}" | \
+		jq -r '. | [
+		.timestamp_ms, 
+		."global options".rw,
+		."global options".ioengine,
+		."global options".direct,
+		."global options".iodepth,
+		.jobs[]."job options".name,
+		.jobs[]."job options".bs,
+		."global options".numjobs,
+		."global options".runtime,
+		.jobs[].read.io_bytes,
+		.jobs[].read.bw_bytes,
+		.jobs[].read.bw_mean,
+		.jobs[].read.iops_mean,
+		.jobs[].write.io_bytes,
+		.jobs[].write.bw_bytes,
+		.jobs[].write.bw_mean,
+		.jobs[].write.iops_mean
+		] | @csv' | sed 's/"//g' >>"${CSV_FILE}"
+	done
+done
+for F in $(ls *.csv); do cat $F | sed 's/,4k,/,0004k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,8k,/,0008k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,16k,/,0016k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,32k,/,0032k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,64k,/,0064k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,128k,/,0128k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,256k,/,0256k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,512k,/,0512k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,1M,/,1024k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,2M,/,2048k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,4M,/,4096k,/g'>${F}.new; mv -f ${F}.new ${F};done
+for F in $(ls *.csv); do cat $F | sed 's/,4M,/,8192k,/g'>${F}.new; mv -f ${F}.new ${F};done
+
